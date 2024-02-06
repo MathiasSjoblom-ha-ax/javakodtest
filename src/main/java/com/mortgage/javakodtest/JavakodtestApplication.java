ce2c855f93f2f4079bc49e0cfc7aedf1388af26a
Mathias Sjöblom <mathias.sjoblom@ha.ax>
2024-02-06 05:31:35 +0200

    init project

---
+++ src/main/java/com/mortgage/javakodtest/JavakodtestApplication.java
@@ -0,0 +1,13 @@
+package com.mortgage.javakodtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavakodtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavakodtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mortgage/javakodtest/JavakodtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mortgage.javakodtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavakodtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
